743232a4a4de8bf9d8eb329a189658a993c51939
NilBG <nil.barrero@estudiant.enti.cat>
2026-01-21 19:37:31 +0100

    EnemySaveData

---
+++ AA2_NilBarreroHectorZornoza/AA2_NilBarreroHectorZornoza/SaveState.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Vector2_.h"
+#define MAX_ENEMIES 50
+
+struct EnemySaveData {
+    Vector2 position;
+    Vector2 room;
+    int hp;
+    bool isDead;
+};
+
+struct SaveState {
+    int playerHP;
+    int playerCoins;
+    int playerPotions;
+    Vector2 playerPos;
+
+    Vector2 currentMapIndex; 
+
+    int enemyCount;
+    EnemySaveData enemies[MAX_ENEMIES];
+};
