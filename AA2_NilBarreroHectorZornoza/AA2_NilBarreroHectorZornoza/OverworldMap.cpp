664f601c919c9fad0d0f3362c7b1981b5b3b5b30
NilBG <nil.barrero@estudiant.enti.cat>
2026-01-30 17:26:47 +0100

    fixed problems

--- AA2_NilBarreroHectorZornoza/AA2_NilBarreroHectorZornoza/OverworldMap.cpp
+++ AA2_NilBarreroHectorZornoza/AA2_NilBarreroHectorZornoza/OverworldMap.cpp
@@ -87,7 +87,7 @@ void OverworldMap::Run(InputSystem& input, Player& player)
 		CC::SetPosition(0, _cellSize.Y + 4 + currentMapOffset.Y);
 		CC::SetColor(CC::WHITE, CC::BLACK);
 		std::cout << "Mapa: [" << _currentMapIndex.X << "," << _currentMapIndex.Y;
-
+		CC::Unlock();
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
 }
